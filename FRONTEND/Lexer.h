e5a847025d01811d3f82a2c19ba33091f5ad80be
Mikhail038 <rachkov.mv@phystech.edu>
2022-12-20 23:01:17 +0300

    Half of Recoursive descent 0.2

--- FRONTEND/Lexer.h
+++
@@ -1,9 +0,0 @@
-DEF_LEX ((Lexem[0] >= L'0' && Lexem[0] <= L'9'),
-{
-    wprintf (L"s\n");
-})
-
-DEF_LEX ((Lexem[0] >= 'A' && Lexem[0] <= 'z'),
-{
-    printf ("s\n");
-})
--- FRONTEND/front.h
+++ FRONTEND/front.h
@@ -1,6 +1,33 @@
 //=============================================================================================================================================================================
+#define TKN_IS_OP TKN.category = COperation
 
-typedef wchar_t CharT;
+#define TKN_OP_AND_IS__ TKN.category == COperation && TKN.type ==
+
+#define CHECK_SYNTAX(tkn_type)  \
+    MTokAss (TKN_OP_AND_IS__ tkn_type); \
+    Tokens->number++;
+
+#define TKN      Tokens->TokenArr[Tokens->number]
+#define TKN_ARGS TKN.category, TKN.type, TKN.data
+
+#define VAR Vars->Arr[counter]
+
+#define FUNC_HEAD_ARGUMENTS STokens* Tokens, SVars* Vars, SFuncs* Funcs
+#define FUNC_ARGUMENTS Tokens, Vars, Funcs
+
+#define SHOUT wprintf (L"%s %s:%d\n", LOCATION)
+
+//===================================================================================================================================================================
+
+void my_main (void);
+
+//===================================================================================================================================================================
+//source//
+//===================================================================================================================================================================
+
+typedef wchar_t CharT; //will not work with char
+
+typedef double  ValT;
 
 typedef struct
 {
@@ -10,35 +37,12 @@ typedef struct
 }
 SSrc;
 
-//===================================================================================================================================================================
-
-enum ETokenType
-{
-    TVariable  = 0,
-    TValue     = 1,
-    TOperation = 2,
-};
-
-typedef struct
-{
-    ETokenType type = TValue;
-}
-SToken;
-
-SToken* lex_src (SSrc* Source);
-
-int do_token (SSrc* Source, SToken* TokenArr);
-
-int parse_token (CharT* Lexem, SToken* TokenArr);
-
-//===================================================================================================================================================================
-
-void _main (void);
-
-//===================================================================================================================================================================
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 SSrc* construct_source (SSrc* Source, FILE* SourceText);
 
+//===================================================================================================================================================================
+//seekers//
 //===================================================================================================================================================================
 
 void seek (SSrc* Source);
@@ -46,4 +50,176 @@ void seek (SSrc* Source);
 void seek_out (SSrc* Source);
 
 //===================================================================================================================================================================
+//lexer//
+//===================================================================================================================================================================
+
+typedef union
+{
+    double  val;
+    CharT*  var;
+} UData;
+
+#define DEF_LEX(d_type, ...) \
+    d_type,
+
+enum ETokenType
+{
+    #include "Lexer.h"
+};
+#undef DEF_LEX
+
+enum ECategory
+{
+    CValue     = 0,
+    CLine      = 1,
+    COperation = 2
+};
+
+typedef struct
+{
+    ECategory  category = CValue;
+    ETokenType type     = TValue;
+    UData      data     = {};
+}
+SToken;
+
+typedef struct
+{
+    int     number;
+    int     size;
+    SToken* TokenArr;
+}
+STokens;
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+STokens* lex_src (SSrc* Source);
+
+int do_token (SSrc* Source, STokens* Tokens);
+
+int parse_token (CharT* Lexem, STokens* Tokens);
+
+void print_tokens (STokens* Tokens);
+
+// void print_token (SToken* Token);
+
+void destruct_tokens (STokens* Tokens);
+
+//=============================================================================================================================================================================
+//number parsers//
+//=============================================================================================================================================================================
+
+double parse_double (CharT* Lexem);
+
+double parse_frac (CharT* Lexem, int* Counter);
+
+double parse_int (CharT* Lexem, int* Counter);
+
+//===================================================================================================================================================================
+//grammar//
+//===================================================================================================================================================================
+
+typedef struct SNode
+{
+    SNode*      left     = NULL;
+    SNode*      right    = NULL;
+    ECategory   category = CValue;
+    ETokenType  type     = TValue;
+    UData       data     = {};
+    SNode*      parent   = NULL;
+}
+SNode;
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+typedef struct
+{
+    CharT*  name    =  NULL;
+    ValT    value   =  0;
+}
+SVar;
+
+typedef struct
+{
+    SVar*   Arr     = NULL;
+    int     number  = 0;
+}
+SVars;
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+typedef struct
+{
+    CharT*  name    =  NULL;
+    SNode*  code    =  NULL;
+}
+SFunc;
+
+typedef struct
+{
+    SFunc*   Arr     = NULL;
+    int     number   = 0;
+}
+SFuncs;
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+SVars* construct_vars_array (size_t Capacity);
+
+void destruct_vars_array (SVars* Vars);
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+SFuncs* construct_funcs_array (size_t Capacity);
+
+void destruct_funcs_array (SFuncs* Funcs);
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+SNode* construct_op_node (ETokenType Type);
+
+SNode* construct_var_node (SToken* Token);
+
+SNode* construct_val_node (ValT Value);
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+SNode* get_All          (FUNC_HEAD_ARGUMENTS);
+
+SNode* get_Statement    (FUNC_HEAD_ARGUMENTS);
+
+SNode* get_Function     (FUNC_HEAD_ARGUMENTS);
+
+SNode* get_Head         (FUNC_HEAD_ARGUMENTS);
+
+SNode* get_Type         (FUNC_HEAD_ARGUMENTS);
+
+SNode* get_Parameters   (FUNC_HEAD_ARGUMENTS);
+
+SNode* get_Param        (FUNC_HEAD_ARGUMENTS);
+
+SNode* get_IfElse       (FUNC_HEAD_ARGUMENTS);
+
+SNode* get_While        (FUNC_HEAD_ARGUMENTS);
+
+SNode* get_Call        (FUNC_HEAD_ARGUMENTS);
+
+SNode* get_Return        (FUNC_HEAD_ARGUMENTS);
+
+SNode* get_Announce     (FUNC_HEAD_ARGUMENTS);
+
+SNode* get_Variable     (FUNC_HEAD_ARGUMENTS);
+
+SNode* get_Expression   (FUNC_HEAD_ARGUMENTS);
+
+ValT get_AddSub     (FUNC_HEAD_ARGUMENTS);
+
+ValT get_MulDiv     (FUNC_HEAD_ARGUMENTS);
+
+ValT get_Pow        (FUNC_HEAD_ARGUMENTS);
+
+ValT get_Bracket    (FUNC_HEAD_ARGUMENTS);
+
+//===================================================================================================================================================================
+
 
